27b6bfa20024efc3b60b7b61b233587098658b0f
ae <90932525+ea-ae@users.noreply.github.com>
2022-08-03 16:25:09 +0300

    Add training data

--- perceptron.cpp
+++ perceptron.cpp
@@ -31,8 +31,8 @@ void perceptron() {
     const auto activationFunction = Relu();
     const auto inputNodes = 784;
     const auto outputNodes = 10;
-    const auto hiddenLayers = 1;
-    const auto hiddenLayerNeurons = 400; // default: 30, 400
+    const auto hiddenLayers = 2;
+    const auto hiddenLayerNeurons = 30; // default: 30, 400
     const auto batchSize = 32;
     const auto learningRate = 0.05; // default: 0.01
 
@@ -40,6 +40,8 @@ void perceptron() {
 
     auto trainingData = std::ifstream("./training-images.idx3-ubyte", std::ios::binary);
     auto trainingLabels = std::ifstream("./training-labels.idx1-ubyte", std::ios::binary);
+    auto testData = std::ifstream("./test-images.idx3-ubyte", std::ios::binary);
+    auto testLabels = std::ifstream("./test-labels.idx1-ubyte", std::ios::binary);
 
     int32_t dataMagic, dataCount, dataRows, dataColumns;
     read<int32_t>(&dataMagic, trainingData);
@@ -60,23 +62,40 @@ void perceptron() {
         for (int i = 0; i < (dataRows * dataColumns); i++) { // for each pixel
             read<uint8_t>(&dataItem.pixels[i], trainingData);
 
-            /*if (nodeValue > 0.8) {
-                std::cout << "X";
-            } else if (nodeValue > 0.2) {
-                std::cout << "x";
-            } else {
-                std::cout << "_";
-            }
+            /*if (nodeValue > 0.8) std::cout << "X";
+            else if (nodeValue > 0.2) std::cout << "x";
+            else std::cout << "_";
             if (i % dataRows == 0) std::cout << "\n";*/
-
         }
         //std::cout << "\n\n";
 
         read<uint8_t>(&dataItem.label, trainingLabels);
     }
 
-    std::cout << std::format("Finished reading {} data items into memory\n", dataItems.size());
+    std::cout << std::format("Finished reading {} training data items into memory\n", dataItems.size());
 
+    // Only weak people stay DRY
+    read<int32_t>(&dataMagic, testData);
+    read<int32_t>(&dataCount, testData);
+    read<int32_t>(&dataRows, testData);
+    read<int32_t>(&dataColumns, testData);
+    std::cout << std::format("Test data magic number: {}\n", dataMagic);
+    std::cout << std::format("Testing with {} images of {}x{} resolution\n", dataCount, dataRows, dataColumns);
+
+    read<int32_t>(&labelMagic, testLabels);
+    read<int32_t>(&labelCount, testLabels);
+    std::cout << std::format("Test labels magic number: {}\n", labelMagic);
+    std::cout << std::format("Test label count: {}\n", labelCount);
+
+    std::vector<DataItem> testItems(dataCount);
+    for (auto& dataItem : testItems) {
+        for (int i = 0; i < (dataRows * dataColumns); i++) {
+            read<uint8_t>(&dataItem.pixels[i], testData);
+        }
+        read<uint8_t>(&dataItem.label, testLabels);
+    }
+
+    std::cout << "Begin training\n";
     int32_t epoch = 1, done = 0, correctGuesses = 0;
     while (true) {
         for (auto& dataItem : dataItems) {
@@ -94,8 +113,21 @@ void perceptron() {
                 perceptron.update(batchSize, learningRate);
             }
         }
-        auto correct = std::ceil((correctGuesses / (double)dataItems.size()) * 10000.0) / 100.0;
-        std::cout << std::format("Epoch {} ({}% correct)\n", epoch, correct);
+
+        int32_t testsPassed = 0;
+        for (auto& dataItem : testItems) { // DRY is a weakness, vol. 2
+            for (int i = 0; i < dataItem.pixels.size(); i++) {
+                double value = dataItem.pixels[i] / 255.0;
+                perceptron.setInputNode(i, value);
+            }
+            perceptron.calculateOutput();
+            if (perceptron.getHighestOutputNode() == dataItem.label) testsPassed++;
+        }
+
+        double dataPassRate = std::ceil((correctGuesses / (double)dataItems.size()) * 10000.0) / 100.0;
+        double testPassRate = std::ceil((testsPassed / (double)testItems.size()) * 10000.0) / 100.0;
+
+        std::cout << std::format("Epoch {} ({}% correct, {}% tests passed)\n", epoch, dataPassRate, testPassRate);
         correctGuesses = 0;
         epoch++;
     }
