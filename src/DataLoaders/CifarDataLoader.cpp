3bcfdf3d851bf8b897d1ae066039b1c7399c00de
ae <90932525+ea-ae@users.noreply.github.com>
2022-08-11 05:31:05 +0300

    Fix file reading bug

--- src/DataLoaders/CifarDataLoader.cpp
+++ src/DataLoaders/CifarDataLoader.cpp
@@ -13,7 +13,7 @@ std::optional<int16_t> CifarDataLoader::loadDataItem(NeuralNetwork& neuralNetwor
 
     for (int i = 0; i < dataItemsIt->pixels->size(); i++) {
         auto pixels = *dataItemsIt->pixels.get();
-        float value = pixels[i] / 255.0f; // [0, 1]
+        float value = static_cast<float>(pixels[i]) / 255.0f; // [0, 1]
         neuralNetwork.setInputNode(i, nthBatchItem, value);
     }
 
@@ -37,10 +37,11 @@ void CifarDataLoader::createDataItems(std::ifstream& data, bool useCoarseLabels)
         dataItem.label = useCoarseLabels ? coarseLabel : fineLabel;
         // std::cout << static_cast<int32_t>(coarseLabel) << " and " << static_cast<int32_t>(fineLabel) << "\n";
 
-        auto pixels = *dataItem.pixels.get();
+        auto& pixels = *dataItem.pixels.get();
         for (size_t p = 0; p < PIXELS_PC; p++) {
             for (size_t c = 0; c < CHANNELS; c++) {
-                read<uint8_t>(&pixels[p + c * PIXELS_PC], data);
+                read<uint8_t>(&pixels[p + c * PIXELS_PC], data); // realign RGB pixels
+                //std::cout << "got " << p + c * PIXELS_PC << " w " << (int)pixels[p + c * PIXELS_PC] << "\n";
             }
         }
     }
